e4b0712e6325ec35f9b71ffb58a8d270c41694c5
freddiemixell <fmixell@gmail.com>
2020-01-26 18:37:55 -0500

    bootstrap autoloader

---
+++ src/bootstrap-autoloader.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger;
+
+// Don't allow loading outside of WordPress.
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Access denied.' );
+}
+
+// Load and register the autoloader.
+require_once __DIR__ . '/Autoloader.php';
+$yidr_autoloader = new Autoloader();
+$yidr_autoloader->add_namespace( __NAMESPACE__, __DIR__ );
+$yidr_autoloader->register();

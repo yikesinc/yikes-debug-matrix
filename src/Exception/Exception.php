e8be62df30550d6c0775f3767f0347721f070a69
freddiemixell <fmixell@gmail.com>
2020-01-26 20:11:41 -0500

    exception interface

---
+++ src/Exception/Exception.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger\Exception;
+
+/**
+ * Interface Exception.
+ *
+ * This interface is implemented by all of this plugin's exceptions so that
+ * we can catch "internal" exceptions only.
+ *
+ * @package YIKES\Debugger\Exception
+ * @author Freddie Mixell
+ */
+interface Exception {
+
+}

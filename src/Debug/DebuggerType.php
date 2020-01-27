ccf6c874f47fa129ac0069282986a17ce385b0d2
freddiemixell <fmixell@gmail.com>
2020-01-27 00:00:37 -0500

    interface for debuggers

---
+++ src/Debug/DebuggerType.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger\Debug;
+
+/**
+ * Interface DebuggerType.
+ *
+ * Properties required for all debuggers.
+ *
+ * @since   0.1.0
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+interface DebuggerType {
+	public function add_message( $message );
+	public function get_messages();
+}

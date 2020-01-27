cb40687c7d3b04a6b0bb93658f155ada82e5f458
freddiemixell <fmixell@gmail.com>
2020-01-26 20:11:52 -0500

    invalid service exception

---
+++ src/Exception/InvalidService.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger\Exception;
+
+/**
+ * Class InvalidService.
+ *
+ * @since   0.1.0
+ *
+ * @package YIKES\Debugger\Exception
+ * @author  Freddie Mixell
+ */
+class InvalidService extends \InvalidArgumentException implements Exception {
+
+	/**
+	 * Create a new instance of the exception for a service class name that is
+	 * not recognized.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $service Class name of the service that was not recognized.
+	 *
+	 * @return static
+	 */
+	public static function from_service( $service ) {
+		$message = sprintf(
+			'The service "%s" is not recognized and cannot be registered.',
+			is_object( $service )
+				? get_class( $service )
+				: (string) $service
+		);
+
+		return new static( $message );
+	}
+}

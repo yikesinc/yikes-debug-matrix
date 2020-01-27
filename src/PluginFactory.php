e868311cdae51e88db13d247444907666392b26e
freddiemixell <fmixell@gmail.com>
2020-01-26 20:18:58 -0500

    avoid singleton classes with shared instance

---
+++ src/PluginFactory.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger;
+
+/**
+ * Class PluginFactory
+ *
+ * @since   0.1.0
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+final class PluginFactory {
+
+	use PluginHelper;
+
+	/**
+	 * Create and return an instance of the plugin.
+	 *
+	 * This always returns a shared instance.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return Plugin The plugin instance.
+	 */
+	public function create() {
+		static $plugin = null;
+
+		if ( null === $plugin ) {
+			$plugin = new Plugin( $this->get_service_container() );
+		}
+
+		return $plugin;
+	}
+
+	/**
+	 * Get the service container for our class.
+	 *
+	 * @since  0.1.0
+	 * @return Container
+	 */
+	private function get_service_container() {
+
+	}
+}

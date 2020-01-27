a126e5424c95a2f438eeceef1093d2e3b6664f43
freddiemixell <fmixell@gmail.com>
2020-01-26 19:17:26 -0500

    containers for services

---
+++ src/Container.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger;
+
+/**
+ * Class Container
+ *
+ * @since   0.1.0
+ * @package YIKES\Debugger
+ */
+final class Container {
+
+	/**
+	 * The registered services for the container.
+	 *
+	 * @since 0.1.0
+	 * @var array
+	 */
+	protected $services = [];
+
+	/**
+	 * Container constructor.
+	 *
+	 * @param array $services Services to register with the container.
+	 */
+	public function __construct( array $services = [] ) {
+		$this->services = $services ?: [];
+	}
+
+	/**
+	 * Get the services from the container.
+	 *
+	 * @since 0.1.0
+	 * @return array
+	 */
+	public function get_services() {
+		return $this->services;
+	}
+
+	/**
+	 * Add a service to the container.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $service Service class name.
+	 */
+	public function add_service( $service ) {
+		$this->services[ $service ] = true;
+	}
+
+	/**
+	 * Remove a service from the container.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param string $service Service class name.
+	 */
+	public function remove_service( $service ) {
+		unset( $this->services[ $service ] );
+	}
+}

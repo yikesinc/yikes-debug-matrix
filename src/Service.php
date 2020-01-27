c94f3e9c3ff4b4b54518a39b9449b4b2abcaed75
freddiemixell <fmixell@gmail.com>
2020-01-26 20:00:08 -0500

    service interface for plugin buckets

---
+++ src/Service.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * YIKES Debugger
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+
+namespace YIKES\Debugger;
+
+/**
+ * Interface Service.
+ *
+ * A generic service.
+ *
+ * @since   0.1.0
+ *
+ * @package YIKES\Debugger
+ * @author  Freddie Mixell
+ */
+interface Service extends Registerable {
+
+}
